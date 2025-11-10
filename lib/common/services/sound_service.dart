b52982135c7c44dac6a090d8d412e30e61297c0b
Martin Winkel <martin@pythomation.de>
2025-11-10 21:17:45 +0100

    feat: sounds on rest screen

--- lib/common/services/sound_service.dart
+++ lib/common/services/sound_service.dart
@@ -1,47 +1,41 @@
-import "package:flutter/services.dart";
+import "package:audioplayers/audioplayers.dart";
 import "package:logging/logging.dart";
 
 class SoundService {
   SoundService._();
   static final SoundService instance = SoundService._();
 
-  static const MethodChannel _channel = MethodChannel("pull_up_club/sounds");
+  final AudioPlayer _countdownPlayer = AudioPlayer();
+  final AudioPlayer _completePlayer = AudioPlayer();
   static final Logger _logger = Logger("SoundService");
 
-  /// Plays a beep sound with the specified frequency and duration
-  ///
-  /// [frequency] - Frequency in Hz (e.g., 800 for countdown, 600 for complete)
-  /// [duration] - Duration in seconds (e.g., 0.1 for countdown, 0.3 for complete)
-  Future<void> _playBeep(final double frequency, final double duration) async {
+  Future<void> playCountdown() async {
     try {
-      await _channel.invokeMethod("playBeep", {
-        "frequency": frequency,
-        "duration": duration,
-      });
+      await _countdownPlayer.play(AssetSource("sounds/beep-countdown.wav"));
     } on Exception catch (e) {
-      _logger.severe("Error while playing beep: $e");
+      _logger.severe("Error while playing countdown sound: $e");
     }
   }
 
-  /// Plays countdown beep (higher pitch, shorter duration)
-  /// Matches the React prototype: playBeep(800, 0.1)
-  Future<void> playCountdown() async {
-    await _playBeep(800, 0.1);
-  }
-
-  /// Plays completion beep (lower pitch, longer duration)
-  /// Matches the React prototype: playBeep(600, 0.3)
   Future<void> playComplete() async {
-    await _playBeep(600, 0.3);
+    try {
+      await _completePlayer.play(AssetSource("sounds/beep-final.wav"));
+    } on Exception catch (e) {
+      _logger.severe("Error while playing complete sound: $e");
+    }
   }
 
-  /// Stops all currently playing sounds
   Future<void> stopAll() async {
     try {
-      await _channel.invokeMethod("stopAll");
+      await _countdownPlayer.stop();
+      await _completePlayer.stop();
     } on Exception catch (e) {
-      // Silently fail if stopping fails
       _logger.severe("Error while stopping all sounds: $e");
     }
+  }
+
+  Future<void> dispose() async {
+    await _countdownPlayer.dispose();
+    await _completePlayer.dispose();
   }
 }
