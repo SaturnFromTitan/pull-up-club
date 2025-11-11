a25fb1b7b6e2c5ad41054b3fbd26c8d45659a8a0
Martin Winkel <martin@pythomation.de>
2025-11-11 23:54:49 +0100

    feat: circular spinner as splash screen

--- lib/common/widgets/splash_screen.dart
+++ lib/common/widgets/splash_screen.dart
@@ -1,10 +1,13 @@
 import "package:flutter/material.dart";
+import "package:pull_up_club/common/themes/app_colors.dart";
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(final BuildContext context) => const Center(
-    child: Image(image: AssetImage("assets/images/app-icon-transparent.png")),
+    child: CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation<Color>(AppColors.onColor),
+    ),
   );
 }
